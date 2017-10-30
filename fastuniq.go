b7eef24ffc064d9a78ad3f373a935359aecf813b
syucream <syucream@mercari.com>
2017-10-30 16:48:25 +0900

    Fix some bugs

--- fastuniq.go
+++ fastuniq.go
@@ -9,52 +9,69 @@ import (
 )
 
 const (
-	BufSize       = 65536
-	HighWaterMark = 4
+	InputBufSize        = 4 * 1024 * 1024
+	OutputMapSize       = 1 * 1024 * 1024
+	IntermediateMapSize = 1 * 1024
+	HighWaterMark       = 2
 )
 
-func process(buf *[]string, wg *sync.WaitGroup, mux *sync.Mutex, result *map[string]int) {
+func process(buf []string, wg *sync.WaitGroup, mux *sync.Mutex, result *map[string]int) {
 	defer wg.Done()
 
-	m := map[string]int{}
-	for _, key := range *buf {
-		m[key] = m[key] + 1
+	// pre-uniq per goroutines
+	m := make(map[string]int, IntermediateMapSize)
+	for _, key := range buf {
+		if v, ok := m[key]; ok {
+			m[key] = v + 1
+		} else {
+			m[key] = 1
+		}
 	}
 
+	// merge uniq values with getting mutex
 	mux.Lock()
 	defer mux.Unlock()
 	for k, v := range m {
-		(*result)[k] = (*result)[k] + v
+		if rv, ok := (*result)[k]; ok {
+			(*result)[k] = rv + v
+		} else {
+			(*result)[k] = v
+		}
 	}
 }
 
 func main() {
+	// sync primitives and a shared map
 	wg := new(sync.WaitGroup)
 	mux := new(sync.Mutex)
-	result := map[string]int{}
+	result := make(map[string]int, OutputMapSize)
 
 	s := bufio.NewScanner(os.Stdin)
+	buf := make([]string, 0, InputBufSize)
 	for {
-		buf := make([]string, BufSize)
+		cursor := buf[:0]
 
 		// read lines until buffer is full
 		isContnue := true
-		for i := 0; i < cap(buf); i++ {
+		for i := 0; i < cap(cursor); i++ {
 			isContnue = s.Scan()
 			if !isContnue {
 				break
 			}
-			buf = append(buf, s.Text())
+			cursor = append(cursor, s.Text())
 		}
+
+		// copy and process buffer
+		// NOTE it has allocation and copy cost ...
+		wg.Add(1)
+		processBuffer := make([]string, len(cursor))
+		copy(processBuffer, cursor)
+		go process(processBuffer, wg, mux, &result)
 
 		// wait if number of goroutines is high
 		if runtime.NumGoroutine() >= HighWaterMark {
 			wg.Wait()
 		}
-
-		// process buffer
-		wg.Add(1)
-		go process(&buf, wg, mux, &result)
 
 		if !isContnue {
 			break
